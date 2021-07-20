763256bb70f63979e41b01e81bb20575479569a0
Diana Zhukova <yndx-dizhukova@yandex.ru>
2021-07-20 22:06:03 +0300

    some edits

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -25,9 +25,9 @@ function AddPlacePopup(props) {
 
     return (
         <PopupWithForm name="add-card" title="Новое место" buttonText="Создать" isOpen={props.isOpen} onClose={props.onClose} onSubmit={handleSubmit} >
-            <input className="popup__input popup__input_card_title" minLength="2" maxLength="30" type="text" id="title-input" name="card-name" placeholder="Название" onChange={handleNameChange} required />
+            <input className="popup__input popup__input_card_title" minLength="2" maxLength="30" type="text" id="title-input" name="card-name" placeholder="Название" value={name} onChange={handleNameChange} required />
             <span className="popup__input-error title-input-error"></span>
-            <input className="popup__input popup__input_card_link" type="url" id="link-input" name="card-link" placeholder="Ссылка на картинку" onChange={handleLinkChange} required />
+            <input className="popup__input popup__input_card_link" type="url" id="link-input" name="card-link" placeholder="Ссылка на картинку" value={link} onChange={handleLinkChange} required />
             <span className="popup__input-error link-input-error"></span>
         </PopupWithForm>
     )
