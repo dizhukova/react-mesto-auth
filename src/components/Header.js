5c6f033f92f3ff816fa15c605e7a09c24c6397e1
Diana Zhukova <yndx-dizhukova@yandex.ru>
2021-07-21 14:19:35 +0300

    feat: add layuot buttons in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 import headerLogo from '../images/logo.svg';
 
-function Header() {
+function Header(props) {
     return (
-    <header className="header">
-        <a href="#" target="_blank">
-            <img className="header__logo" src={headerLogo} alt="Логотип Mesto" />
+        <header className="header">
+            {props.loggedIn &&
+                <div className="header__auth-info">
+                    <p className="header__email">{props.email}</p>
+                    <button className="header__log-out" type="submit" name="submit">Выйти</button>
+                </div>
+            }
+            <a href="#" target="_blank">
+                <img className="header__logo" src={headerLogo} alt="Логотип Mesto" />
             </a>
-    </header>
+            <Route path="/sign-up">
+                <Link className="header__auth-button" to="sign-in">Войти</Link>
+            </Route>
+            <Route path="/sign-in">
+                <Link className="header__auth-button" to="sign-up">Регистрация</Link>
+            </Route>
+            {/* <Route path="/">
+                <div className="header__auth-info">
+                    <p className="header__email">{props.email}</p>
+                    <button className="header__log-out" type="submit" name="submit">Выйти</button>
+                </div>
+            </Route> */}
+        </header>
     )
 }
 
 export default Header;
+
